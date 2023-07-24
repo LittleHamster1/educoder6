011a95368ae22c61f7667bc729eba9446b1d610c
LittleHamster1 <124270898+LittleHamster1@users.noreply.github.com>
2023-07-24 15:50:08 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,78 @@
+﻿/********** BEGIN **********/
+#include "Int.h"
+
+Int::Int() :value(0) {}
+
+Int::Int(const Int& rhs) :value(rhs.value) {}
+
+Int::Int(int v) :value(v) {}
+
+Int::~Int() {}
+
+int Int::getValue()const { return this->value; }
+
+void Int::setValue(int v) { this->value = v; }
+
+Int& Int::operator = (const Int& rhs)
+{
+    if (this != &rhs)
+    {
+        this->setValue(rhs.getValue());
+    }
+    return *this;
+}
+
+Int& Int::operator += (const Int& rhs)
+{
+    this->setValue(this->getValue() + rhs.getValue());
+    return *this;
+}
+
+Int& Int::operator -= (const Int& rhs)
+{
+    this->setValue(this->getValue() - rhs.getValue());
+    return *this;
+}
+
+Int& Int::operator *= (const Int& rhs)
+{
+    this->setValue(this->getValue() * rhs.getValue());
+    return *this;
+}
+
+Int& Int::operator /= (const Int& rhs)
+{
+    this->setValue(this->getValue() / rhs.getValue());
+    return *this;
+}
+
+Int& Int::operator %= (const Int& rhs)
+{
+    this->setValue(this->getValue() % rhs.getValue());
+    return *this;
+}
+
+Int& Int::operator++()
+{
+    return *this += Int(1);
+}
+
+Int& Int::operator--()
+{
+    return *this -= Int(1);
+}
+
+Int Int::operator++(int)
+{
+    Int ret(*this);
+    ++* this;
+    return ret;
+}
+
+Int Int::operator--(int)
+{
+    Int ret(*this);
+    --* this;
+    return ret;
+}
+/********** END **********/
